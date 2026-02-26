8e0ca19052fe0e9deaa1f03a493210fc15ed1522
TheKlif <39699416+TheKlif@users.noreply.github.com>
2026-02-26 06:49:08 -0500

    publish update

--- publish.ps1
+++ publish.ps1
@@ -1,4 +1,4 @@
-# Publish script (markdown from external Obsidian publish folder)
+# Publish script (external Obsidian + slugified filenames)
 
 $source = "D:\Obsidian\Klif-Brain\Publish"
 
@@ -6,7 +6,12 @@ Get-ChildItem -Path $source -Recurse -Include "*.md" | ForEach-Object {
     $relative = $_.FullName.Substring($source.Length + 1)
     $targetDir = Join-Path ".\Is-This-Anything" (Split-Path $relative)
     if (!(Test-Path $targetDir)) { New-Item -ItemType Directory -Path $targetDir }
-    $output = Join-Path $targetDir ($_.BaseName + ".html")
+
+    # slugify filename: remove extension, replace spaces with hyphens, lowercase
+    $slug = $_.BaseName -replace ' ', '-'
+    $slug = $slug.ToLower()
+
+    $output = Join-Path $targetDir ($slug + ".html")
     pandoc $_.FullName -o $output
 }
 
