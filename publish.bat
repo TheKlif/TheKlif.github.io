3347244eacebba1d32cf662f55fa3bf955401c5c
TheKlif <39699416+TheKlif@users.noreply.github.com>
2026-02-27 05:51:45 -0500

    publish update

--- publish.bat
+++ publish.bat
@@ -1,3 +1,8 @@
 @echo off
 cd /d "D:\Is This Anything\TheKlif.github.io"
+
 powershell.exe -ExecutionPolicy Bypass -File "publish.ps1" > "%~dp0publish.log" 2>&1
+
+if %errorlevel%==0 (
+    powershell -Command "$xml='<toast><visual><binding template=\"ToastGeneric\"><text>Publish complete</text></binding></visual></toast>'; $doc=New-Object Windows.Data.Xml.Dom.XmlDocument; $doc.LoadXml($xml); $toast=[Windows.UI.Notifications.ToastNotification]::new($doc); [Windows.UI.Notifications.ToastNotificationManager]::CreateToastNotifier('Is This Anything').Show($toast)"
+)
