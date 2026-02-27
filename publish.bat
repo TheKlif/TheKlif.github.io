c67d723a9e5e9ed6e6d898ab7666f8dc5125733c
TheKlif <39699416+TheKlif@users.noreply.github.com>
2026-02-27 05:53:38 -0500

    publish update

--- publish.bat
+++ publish.bat
@@ -5,4 +5,6 @@ powershell.exe -ExecutionPolicy Bypass -File "publish.ps1" > "%~dp0publish.log" 
 
 if %errorlevel%==0 (
     powershell -Command "$xml='<toast><visual><binding template=\"ToastGeneric\"><text>Publish complete</text></binding></visual></toast>'; $doc=New-Object Windows.Data.Xml.Dom.XmlDocument; $doc.LoadXml($xml); $toast=[Windows.UI.Notifications.ToastNotification]::new($doc); [Windows.UI.Notifications.ToastNotificationManager]::CreateToastNotifier('Is This Anything').Show($toast)"
+) else (
+    powershell -Command "$xml='<toast><visual><binding template=\"ToastGeneric\"><text>Publish failed</text></binding></visual></toast>'; $doc=New-Object Windows.Data.Xml.Dom.XmlDocument; $doc.LoadXml($xml); $toast=[Windows.UI.Notifications.ToastNotification]::new($doc); [Windows.UI.Notifications.ToastNotificationManager]::CreateToastNotifier('Is This Anything').Show($toast)"
 )
